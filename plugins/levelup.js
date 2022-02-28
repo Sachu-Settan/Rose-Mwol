f71aa740ee9ece47ed9a5b75b1e4b6e7aa477fd3
Sachu <93819264+Sachu-Settan@users.noreply.github.com>
2022-02-28 07:41:13 +0530

    ʀᴏꜱᴇ-ᴍᴡᴏʟ

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (!levelling.canLevelUp(user.level, user.exp, global.multiplier)) {
       let rank = 'https://telegra.ph/file/94996a70d9de31b16793c.jpg'
         {
-          await conn.sendButtonImg(m.chat, await (await fetch(rank)).buffer(), `Level ${name} ${user.level} (${user.exp - min}/${xp})\nNot enough ${max - user.exp} again!`.trim(), '© Eva', 'AUTO LEVEL UP', `${usedPrefix}on autolevelup`, m)
+          await conn.sendButtonImg(m.chat, await (await fetch(rank)).buffer(), `Level ${name} ${user.level} (${user.exp - min}/${xp})\nNot enough ${max - user.exp} again!`.trim(), '© ʀᴏꜱᴇ ᴍᴡᴏʟ', 'AUTO LEVEL UP', `${usedPrefix}on autolevelup`, m)
         }
     }
     let before = user.level * 1
@@ -28,7 +28,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (before !== user.level) {
       let rank = 'https://telegra.ph/file/94996a70d9de31b16793c.jpg'
         {
-          await conn.sendButtonImg(m.chat, await (await fetch(rank)).buffer(), `${name} Level Up!\n_${before}_ -> ${user.level}`.trim(), '© Rose Mwol', 'AUTO LEVEL UP', `${usedPrefix}on autolevelup`, m)
+          await conn.sendButtonImg(m.chat, await (await fetch(rank)).buffer(), `${name} Level Up!\n_${before}_ -> ${user.level}`.trim(), '© ʀᴏꜱᴇ ᴍᴡᴏʟ', 'AUTO LEVEL UP', `${usedPrefix}on autolevelup`, m)
         }
     }
   }
